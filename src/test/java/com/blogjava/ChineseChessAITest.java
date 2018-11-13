7be9bd46b928e9d71d754924797d4b40a0d8d6b8
welldoer <welldoer@263.net>
2018-11-13 16:40:11 +0800

    按照AssertJ Swing的标准测试代码建议，完善测试代码。

--- src/test/java/com/blogjava/ChineseChessAITest.java
+++ src/test/java/com/blogjava/ChineseChessAITest.java
@@ -2,20 +2,33 @@ package com.blogjava;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ChineseChessAITest {
 	private ChineseChessAI chineseChessAI;
+	private FrameFixture window;
 
+	@BeforeClass
+	public static void setUpOnce() throws Exception {
+		FailOnThreadViolationRepaintManager.install();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
-		chineseChessAI = new ChineseChessAI();
+		chineseChessAI = GuiActionRunner.execute( () -> new ChineseChessAI() );
+		window = new FrameFixture( chineseChessAI );
+		window.show();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		window.cleanUp();
 	}
 
 	@Test
