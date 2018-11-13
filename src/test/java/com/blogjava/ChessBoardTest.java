b37d35c53531e7831ca8c4bc6d99f582a4bdbdb7
welldoer <welldoer@263.net>
2018-11-13 21:47:54 +0800

    针对Travis-CI中报org.assertj.swing.exception.EdtViolationException，尝试进行修复

--- src/test/java/com/blogjava/ChessBoardTest.java
+++ src/test/java/com/blogjava/ChessBoardTest.java
@@ -2,12 +2,19 @@ package com.blogjava;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ChessBoardTest {
 	private ChessBoard chessBoard;
 
+	@BeforeClass
+	public static void setUpOnce() throws Exception {
+		FailOnThreadViolationRepaintManager.install();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
 		chessBoard = new ChessBoard();
