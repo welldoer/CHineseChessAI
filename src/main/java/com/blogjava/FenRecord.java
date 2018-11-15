454ba67bda41212cedaa0dbb7fdf98c307f5cafc
welldoer <welldoer@263.net>
2018-11-15 15:31:08 +0800

    创建简单的Fen类。

---
+++ src/main/java/com/blogjava/FenRecord.java
@@ -0,0 +1,12 @@
+package com.blogjava;
+
+public class FenRecord {
+
+	public FenRecord( String string ) {
+	}
+
+	public boolean isRedGo() {
+		return true;
+	}
+
+}
---
+++ src/test/java/com/blogjava/FenRecordTest.java
@@ -0,0 +1,23 @@
+package com.blogjava;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FenRecordTest {
+	private FenRecord fenOrigRecord, fenSimpleRecord;
+
+	@Before
+	public void setUp() throws Exception {
+		fenOrigRecord = new FenRecord( "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1" );
+		fenSimpleRecord = new FenRecord( "1r7/2c6/9/9/9/9/9/9/9/9 b - - 20 50" );
+	}
+
+	@Test
+	public void testFenSimpleRecord() {
+		assertThat( fenOrigRecord.isRedGo() ).isEqualTo( true );
+		assertThat( fenSimpleRecord.isRedGo() ).isEqualTo( true );
+	}
+
+}
